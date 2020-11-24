367445dc228be63bf94f6df2bb0aa3b5fc90bb53
Jan Moritz Pocher <j.pocher@campus.tu-berlin.de>
2020-11-24 20:33:25 +0100

    Created a first draft of the inventory class

---
+++ Inventory.java
@@ -0,0 +1,58 @@
+import java.util.ArrayList;
+
+public class Inventory {
+
+    private int inventorySize;
+    private ArrayList<Fish> fishArr;
+    private ArrayList<Rod> rodArr;
+    private ArrayList<Bait> baitArr;
+    private ArrayList<Integer> baitAmountArr;
+
+    public Inventory() {
+        this.inventorySize = 15;
+    }
+
+    public void addBait(Bait bait) {
+        if(baitArr.contains(bait)) {
+            baitAmountArr.set(baitArr.indexOf(bait), baitAmountArr.get(baitArr.indexOf(bait)) + 1);
+        }
+        else {
+            baitArr.add(bait);
+            baitAmountArr.add(1);
+        }
+    }
+
+    public useBait(Bait bait) {
+        baitAmountArr.set(baitArr.indexOf(bait), baitAmountArr.get(baitArr.indexOf(bait)) - 1);
+    }
+
+    public ArrayList<Integer> getBaitAmounts() {
+        return baitAmountArr;
+    }
+
+    public ArrayList<Bait> getBaitList() {
+        return baitArr;
+    }
+
+
+    public void addRod(Rod rod) {
+        if(rodArr.contains(rod)) {
+            System.out.println("No need to add an existing rod. This particular rod should not be purchasable!");
+        }
+        else {
+            rodArr.add(rod);
+        }
+    }    
+
+
+
+    public void addFishToInventory(Fish fish) {
+        fishArr.push(fish);
+    }
+
+    public int getInventoryFishAmount() {
+        //return InventoryFishAmount 
+        return fishArr.length;
+    }
+
+}
