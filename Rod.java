23e86f2774bd59e5a49cb53c7378f5729cde09b3
Jan Moritz Pocher <j.pocher@campus.tu-berlin.de>
2020-11-25 09:04:00 +0100

    Added a basic rod class

---
+++ Rod.java
@@ -0,0 +1,18 @@
+public class Rod {
+    private String baseName;
+    private int level;
+    
+    public Rod(String name, int level) {
+        this.baseName = name;
+        this.level = level;
+    }
+
+    public int getLevel() {
+        return level;
+    }
+
+    public String getFullName() {
+        return baseName + String.format(" Level %d", level);
+    }
+
+}
