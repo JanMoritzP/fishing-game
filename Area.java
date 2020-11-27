24bc4a5ba11a2d39dc549ee6825f6969204ea91f
Jan Moritz Pocher <j.pocher@campus.tu-berlin.de>
2020-11-27 17:39:51 +0100

    Added basic infrastructure of json parser

--- Area.java
+++ Area.java
@@ -1,9 +1,15 @@
 import java.time.LocalTime;
+import java.io.FileNotFoundException;
+import java.io.FileReader;
+import java.io.IOException;
+import java.util.Iterator;
+import org.json.simple.*;
+import org.json.simple.parser.JSONParser;
+import org.json.simple.parser.ParseException;
 
 public class Area {
 
     private String name;
-
 
     public Area(String name) {
         this.name = name;
@@ -13,12 +19,19 @@ public class Area {
         return LocalTime.now().toString().substring(0, 8);
     }
 
-
     public String getName() {
         return name;
     }
 
-    public Fish createFish() {  //Maybe this should be Fish[] or ArrayList<Fish>
+    public Fish createFish() throws FileNotFoundException, IOException, ParseException { // Maybe this should be Fish[] or ArrayList<Fish>
+
+        JSONObject jo = (JSONObject) new JSONParser().parse(new FileReader("scr/Fish.json"));
+        JSONArray ja = (JSONArray) jo.get("fish");
+        Iterator itr2 = ja.iterator();
+        
+
+        
+
         return null;  //Placeholder
     }
 
