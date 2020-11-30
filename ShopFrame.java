963c1a5be063dbdea6ec386167ce340dffe0643d
Ivylux <ivylux.dev@gmail.com>
2020-11-30 17:40:49 +0100

    Working on shop, see MainFrame.java line 38-41 and ShopFrame.java

---
+++ ShopFrame.java
@@ -0,0 +1,13 @@
+
+import javax.swing.*;
+import java.awt.*;
+import java.awt.event.*;
+
+public class ShopFrame 
+{
+    public static void main(String[] args)
+    {
+        MainFrame mf = new MainFrame();
+        mf.setVisible(true);
+    }  
+}
