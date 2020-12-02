5e5448b9b459e4fe2685b73370c33401859ec7c4
Jan Moritz Pocher <j.pocher@campus.tu-berlin.de>
2020-12-02 09:09:43 +0100

    Changed a small line to fix bug

--- FishGamePanel.java
+++ FishGamePanel.java
@@ -33,8 +33,8 @@ public class FishGamePanel extends JPanel {
                     }
                     else {
                         fishIterator.next();
+                        i++;
                     }
-                    i++;
                 }
             }
         });
