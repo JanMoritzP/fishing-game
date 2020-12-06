fb9d8b29236a6dd57bf56108114c0e68ae987ced
Jan Moritz Pocher <j.pocher@campus.tu-berlin.de>
2020-12-06 16:13:43 +0100

    Deleted obsolete Fish addition

--- MainFrame.java
+++ MainFrame.java
@@ -30,18 +30,7 @@ class MainFrame extends JFrame implements ActionListener
         setLayout(null);
         setResizable(false);
         setDefaultCloseOperation(WindowConstants.EXIT_ON_CLOSE);
-        ///*
-        playerInventory.addFish(new Fish("trout",5,5));
-        playerInventory.addFish(new Fish("trout",5,5));
-        playerInventory.addFish(new Fish("trout",5,5));
-        playerInventory.addFish(new Fish("trout",5,5));
-        playerInventory.addFish(new Fish("trout",5,5));
-        playerInventory.addFish(new Fish("trout",5,5));
-        playerInventory.addFish(new Fish("trout",5,5));
-        playerInventory.addFish(new Fish("trout",5,5));
-        playerInventory.addFish(new Fish("trout",5,5));
-        playerInventory.addFish(new Fish("trout",5,5));
-        //*/
+        
         initComponent();
     }
 
